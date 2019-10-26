6596241f8140a27ad6190baafdfb7bb4bf0b8144
Hong Ngoc Do, Stephanie Rodriguez, Amobi Somto <natalie.mimi@gmail.com>
2019-10-26 18:45:15 -0500

    add

--- Provid/ViewController.swift
+++ Provid/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"background.png"]]];
 
 }
 
