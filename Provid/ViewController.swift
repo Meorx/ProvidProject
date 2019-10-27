f06b6b4056e0537ad381d52373d595fc32946bb3
Hong Ngoc Do, Stephanie Rodriguez, Amobi Somto <natalie.mimi@gmail.com>
2019-10-27 00:55:24 -0500

    The app is able to browse through user's photos successfully.

--- Provid/ViewController.swift
+++ Provid/ViewController.swift
@@ -9,16 +9,58 @@
 import UIKit
 import WebKit // possibly for the javascript reference:
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var desiredRatio: UITextField!
+    @IBOutlet weak var desiredLength: UITextField!
+    @IBOutlet weak var desiredWidth: UITextField!
+    @IBOutlet var testButton: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    //@IBOutlet var imagePreview : UIImageView
+
+    @IBAction func AddImageButton(UIButton : UIButton) {
+        imagePicker.modalPresentationStyle = UIModalPresentationStyle.currentContext
+        imagePicker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        //self.presentModalViewController(imagePicker, animated: true)
+        self.present(imagePicker, animated: true, completion: nil)
+    
+    }
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info:NSDictionary!) {
+        let tempImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        // this is your image - use it for cloud or wehatever here
+        print(tempImage)
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+
+    func imagePickerControllerDidCancel(picker: UIImagePickerController!) {
+
+self.dismiss(animated: true, completion: nil)//        self.performsActionsWhilePresentingModally(true)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        desiredRatio.delegate = self
+        desiredLength.delegate = self
+        desiredWidth.delegate = self
+        
+        let alert = UIAlertController(title: "My Alert", message: "This is an alert.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+        NSLog("The \"OK\" alert occured.")
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
         // Do any additional setup after loading the view.
+        
     }
     
     //func readLine(strippingNewline: Bool = true) -> String?
-
-
+    
 }
 
 
